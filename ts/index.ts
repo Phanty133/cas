43bd2a2bac0b6764e28af28ba4e4f8b3b9bcc6f8
Phanty133 <vintersk@gmail.com>
2022-06-01 16:23:05 +0300

    end screen

--- ts/index.ts
+++ ts/index.ts
@@ -24,6 +24,10 @@ function init() {
 		headlines = resp; // load headlines
 		document.getElementById("totalQuestions")!.innerText = headlines.length.toString();
 	});
+
+	addQuestionDropdown("q1", false, true, "hello1");
+	addQuestionDropdown("q2", true, true, "hello2");
+	addQuestionDropdown("q3", true, false, "hello3");
 }
 
 function start() {
@@ -57,6 +61,30 @@ function getMistakes() {
 		return `<b>${x.headline}</b> was ${x.isTrue ? "True" : "False"}\nYou answered: ${!x.isTrue ? "True" : "False"}\nReason:\n${x.explanation}`;
 	});
 	return mistakeHTML;
+}
+
+function addQuestionDropdown(q: string, isTrue: boolean, wasCorrect: boolean, explanation: string) {
+	const container = document.getElementById("questionDropdownContainer") as HTMLDivElement;
+
+	const correctPercentage = 69;
+
+	const dropdownEl = document.createElement("div");
+	dropdownEl.className = "questionDropdown";
+	dropdownEl.setAttribute(wasCorrect ? "data-correct" : "data-wrong","");
+
+	dropdownEl.innerHTML = `
+	<span class="dropdownHeadline">${q}</span>
+	<span class="dropdownHeadlineAnswer">${isTrue ? "Real" : "Fake"}</span>
+
+	<div class="dropdownContent">
+		<span>${explanation}</span>
+		<br><br>
+		<span class="questionDropdownCorrectPercentage"><b>${correctPercentage}%</b> answered correctly</span>
+	</div>`;
+
+	dropdownEl.addEventListener("click", () => { dropdownEl.toggleAttribute("data-active"); });
+
+	container.appendChild(dropdownEl);
 }
 
 if (document.readyState === "complete" || document.readyState === "interactive") {
