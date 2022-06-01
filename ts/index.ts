e93e817a4aa9434e0d4e5c45a46123f5db5d4ed1
tower <towergame.epic@gmail.com>
2022-06-01 14:06:42 +0300

    Basic game logic

--- ts/index.ts
+++ ts/index.ts
@@ -1,5 +1,53 @@
+class Headline {
+	headline: string = "";
+	isTrue: boolean = false;
+	explanation: string = "";
+}
+
+let currIndex = 0;
+let headlines: Headline[] = [];
+let incorrect: Headline[] = [];
+
+// Fisher-Yates based mode doe
+function shuffle(arr: any[]) {
+	for (let i = 0; i < arr.length-1; i++) {
+		let j = Math.floor(Math.random() * (arr.length - i - 1)) + i;
+		let temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+	return arr;
+}
+
 function init() {
-    console.log("Hello world!");
+	fetch("headlines.json").then(resp => resp.json()).then(resp => {
+		headlines = resp; // load headlines
+	});
+}
+
+function start() {
+	currIndex = 0;
+	incorrect = [];
+	headlines = shuffle(headlines); // shuffle headlines
+	// TODO: scene transition
+}
+
+function getNext() {
+	return headlines[currIndex].headline;
+}
+
+function answer(answer: boolean) {
+	if(headlines[currIndex].isTrue !== answer) {
+		incorrect.push(headlines[currIndex]);
+	}
+	currIndex++;
+}
+
+function getMistakes() {
+	let mistakeHTML = incorrect.map(x => {
+		return `<b>${x.headline}</b> was ${x.isTrue ? "True" : "False"}\nYou answered: ${!x.isTrue ? "True" : "False"}\nReason:\n${x.explanation}`;
+	});
+	return mistakeHTML;
 }
 
 if (document.readyState === "complete" || document.readyState === "interactive") {
